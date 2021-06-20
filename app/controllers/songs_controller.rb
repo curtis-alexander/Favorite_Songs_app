a8b2843826397a4dc5b9aa1889cbc0a23c81cac9
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-06-20 12:26:03 -0500

    add creat action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,17 @@
 class SongsController < ApplicationController
   def index
-    render json: { message: "hello" }
+    song = Song.all
+    render json: song.as_json
+  end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      artist: params[:artist],
+      album: params[:album],
+      year: params[:year],
+    )
+    song.save
+    render json: song.as_json
   end
 end
